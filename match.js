b4ec373b6bea5642efb8aedf643733f7522d275e
Daniel-Davis <ddv9461@saintjoe.edu>
2015-03-30 12:13:20 -0500

    Update match.js

--- match.js
+++ match.js
@@ -1,5 +1,5 @@
 var input = "A string with 3 numbers in it... 42 and 88.";
-var number = /\b(\d+\d+)\b/g;
+var number = /\b(\d\d)\b/g; // modified so that way it only takes double digit numbers
 var match;
 while (match = number.exec(input))
   console.log("Found", match[1], "at", match.index);
